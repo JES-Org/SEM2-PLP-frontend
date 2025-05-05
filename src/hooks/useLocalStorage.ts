c3d87431f9ee7f009906b41cf9af9f3ab425bb34
jenberu <jemberu0970@gmail.com>
2025-05-05 10:44:31 +0300

    Refactor useLocalStorage hook to improve browser check and error handling

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,30 +1,36 @@
 'use client'
 
 export const useLocalStorage = (key: string) => {
-    const setItem = (value: unknown) => {
-      try {
-        window.localStorage.setItem(key, JSON.stringify(value));
-      } catch (error) {
-        console.log(`useLocalStorage error: ${error}`)
-      }
-    };
-  
-    const getItem = () => {
-      try {
-        const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : undefined;
-      } catch (error) {
-        console.log(`useLocalStorage error: ${error}`)
-      }
-    };
-  
-    const removeItem = () => {
-      try {
-        window.localStorage.removeItem(key);
-      } catch (error) {
-        console.log(`useLocalStorage error: ${error}`)
-      }
-    };
-  
-    return { setItem, getItem, removeItem };
-  };
+  const isBrowser = typeof window !== 'undefined'
+
+  const setItem = (value: unknown) => {
+    if (!isBrowser) return
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value))
+    } catch (error) {
+      console.log(`useLocalStorage error: ${error}`)
+    }
+  }
+
+  const getItem = () => {
+    if (!isBrowser) return undefined
+    try {
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item) : undefined
+    } catch (error) {
+      console.log(`useLocalStorage error: ${error}`)
+      return undefined
+    }
+  }
+
+  const removeItem = () => {
+    if (!isBrowser) return
+    try {
+      window.localStorage.removeItem(key)
+    } catch (error) {
+      console.log(`useLocalStorage error: ${error}`)
+    }
+  }
+
+  return { setItem, getItem, removeItem }
+}
