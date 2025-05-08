20b485a880e3a9759176fca0da8145685eec905e
jenberu <jemberu0970@gmail.com>
2025-05-09 01:48:24 +0300

    Fix base URL in notificationApi to correct API endpoint

--- src/store/notification/notificationApi.ts
+++ src/store/notification/notificationApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const notificationApi = createApi({
   reducerPath: "notificationApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5275/api/notifications",
+    baseUrl: "http://localhost:8000/api/notifications",
     prepareHeaders: (headers) => {
       const token = JSON.parse(localStorage.getItem('currUser')!).token as string;
       if (token) {
