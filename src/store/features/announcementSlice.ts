423f03fc14715cd695af96a7a8565922751f3fdf
jenberu <jemberu0970@gmail.com>
2025-05-17 02:18:01 +0300

    Add selectedAnnouncement state and corresponding reducer for announcementSlice

--- src/store/features/announcementSlice.ts
+++ src/store/features/announcementSlice.ts
@@ -1,17 +1,26 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Annoucement } from '@/types/announcement/announcement.type'
 
 interface announcementState {
 	currAnnouncementId: string
+	selectedAnnouncement: Annoucement | null
 }
 
 const initialState: announcementState = {
 	currAnnouncementId: '',
+	selectedAnnouncement: null,
 }
 
 const announcementSlice = createSlice({
 	name: 'announcement',
 	initialState,
 	reducers: {
+		setSelectedAnnouncement: (
+			state,
+			action: PayloadAction<Annoucement | null>,
+		) => {
+			state.selectedAnnouncement = action.payload
+		},
 		setAnnouncementId: (state, action: PayloadAction<string>) => {
 			state.currAnnouncementId = action.payload
 		},
@@ -21,6 +30,8 @@ const announcementSlice = createSlice({
 export const selectAnnouncementId = (state: {
 	announcement: announcementState
 }) => state.announcement.currAnnouncementId
+export const selectSelectedAnnouncement = (state: any) => state.announcement.selectedAnnouncement;
 
-export const { setAnnouncementId } = announcementSlice.actions
+
+export const { setAnnouncementId,setSelectedAnnouncement } = announcementSlice.actions
 export default announcementSlice.reducer
