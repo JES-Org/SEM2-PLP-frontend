899b3e5a4808114e894c0b2a755c2a745fc80f8e
jenberu <jemberu0970@gmail.com>
2025-05-17 02:18:53 +0300

    Refactor UnreadNotificationData interface to standardize property names and types

--- src/types/notification/notification.type.ts
+++ src/types/notification/notification.type.ts
@@ -1,15 +1,16 @@
 export interface UnreadNotificationResponse {
   isSuccess?: boolean;
-  message?:   string | null;
-  data:      UnreadNotificationData[];
-  errors?:    string[] | null;
+  message?: string | null;
+  data: UnreadNotificationData[];
+  errors?: string[] | null;
 }
 
 export interface UnreadNotificationData {
-  id?:          string;
-  description: string;
-  receivers?:   any[];
-  classroomId?: string;
-  createdAt?:   Date;
-  updatedAt?:   Date;
+  id: number;                    
+  message: string;                
+  url?: string | null;             
+  is_read: boolean;                
+  created_at: string;            
+  sender?: number | null;        
+  recipient: number;              
 }
