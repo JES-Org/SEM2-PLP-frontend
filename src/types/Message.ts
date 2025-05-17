db36d9d31129aefc6d66b2157d3ca424811de2c5
jenberu <jemberu0970@gmail.com>
2025-05-17 22:49:13 +0300

    Enhance MessageType and ApiMessageType interfaces with additional properties for improved message handling

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -1,4 +1,20 @@
 export interface MessageType {
+	id: string;
   text: string;
-  sender: 'other' | 'me'
+  sender: 'me' | 'other';
+  senderName?: string;
+  timestamp?: string;
+  isRead?: boolean;
+  readBy?: string[];
+
+}
+
+
+export interface ApiMessageType {
+  id: number | string; 
+  content: string;
+  sender_id: number | string; 
+  sender_name: string;
+  timestamp: string; 
+  is_read_by_current_user?: boolean; 
 }
