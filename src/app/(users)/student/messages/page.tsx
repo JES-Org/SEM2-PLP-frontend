51870f2f4345498f27ed381c46bc2b02bb37471c
jenberu <jemberu0970@gmail.com>
2025-05-19 01:33:32 +0300

    Refactor MessagesPage component: replace StudentMessageDashboard with MessageDashboard and adjust layout

--- src/app/(users)/student/messages/page.tsx
+++ src/app/(users)/student/messages/page.tsx
@@ -1,9 +1,9 @@
-import { StudentMessageDashboard } from '@/components/messaging'
+import { MessageDashboard } from '@/components/messaging'
 
 export default function MessagesPage() {
     return (
-        <div className='ml-10 mr-24'>
-            <StudentMessageDashboard />
+        <div className='ml-10'>
+            <MessageDashboard />
         </div>
     )
 }
