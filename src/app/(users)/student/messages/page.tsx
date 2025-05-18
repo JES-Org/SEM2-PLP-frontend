5440aa9738d9aba1af0857136f3657a561b96074
jenberu <jemberu0970@gmail.com>
2025-05-18 11:56:07 +0300

    Add MessagesPage component to display student message dashboard

---
+++ src/app/(users)/student/messages/page.tsx
@@ -0,0 +1,9 @@
+import { StudentMessageDashboard } from '@/components/messaging'
+
+export default function MessagesPage() {
+    return (
+        <div className='ml-10 mr-24'>
+            <StudentMessageDashboard />
+        </div>
+    )
+}
