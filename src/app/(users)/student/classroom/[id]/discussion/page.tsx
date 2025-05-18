34843b5bd5304ea08986c819c43025a053102170
jenberu <jemberu0970@gmail.com>
2025-05-19 01:33:08 +0300

    Enhance Forum component: set height for discussion container to improve layout

--- src/app/(users)/student/classroom/[id]/discussion/page.tsx
+++ src/app/(users)/student/classroom/[id]/discussion/page.tsx
@@ -7,7 +7,7 @@ import DiscussionChat from '@/components/DiscussionChat'
 const Forum = () => {
 	const [typing, setTyping] = useState(false)
 	return (
-		<div className='md:pl-72'>
+		<div className='md:pl-72 h-[calc(100vh-5rem)]'>
 			<DiscussionChat typing={typing} />
 		</div>
 	)
