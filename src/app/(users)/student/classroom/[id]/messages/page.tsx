d3f24af122c3a5817c41d950c01869c699193505
jenberu <jemberu0970@gmail.com>
2025-05-18 11:55:49 +0300

    Add ClassroomChatPage component to display student messages

---
+++ src/app/(users)/student/classroom/[id]/messages/page.tsx
@@ -0,0 +1,11 @@
+import { StudentMessageDashboard } from '@/components/messaging'
+
+export default function ClassroomChatPage() {
+  
+  return (
+    <div className='ml-10 mr-24 mt-10'>
+      <StudentMessageDashboard />
+    </div>
+
+  )
+}
