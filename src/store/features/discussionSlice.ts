a0d2deda909b36743aba6f372d7355ce007b9070
jenberu <jemberu0970@gmail.com>
2025-05-19 01:39:31 +0300

    Refactor discussionSlice: improve code formatting and readability

--- src/store/features/discussionSlice.ts
+++ src/store/features/discussionSlice.ts
@@ -1,44 +1,55 @@
-import { CreateMessageResponseData } from "@/types/discussion/discussion.type";
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { CreateMessageResponseData } from '@/types/discussion/discussion.type'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 interface isRightClickType {
-  id: string | null
-  content: string
-  option: 'edit' | 'delete' | null
+	id: string | null
+	content: string
+	option: 'edit' | 'delete' | null
 }
 
 interface DiscussionState {
-  messages: CreateMessageResponseData[]
-  isRightClicked: isRightClickType
+	messages: CreateMessageResponseData[]
+	isRightClicked: isRightClickType
 }
 
 const initialState: DiscussionState = {
-  messages: [],
-  isRightClicked: {
-    id: null,
-    content: '',
-    option: null
-  }
+	messages: [],
+	isRightClicked: {
+		id: null,
+		content: '',
+		option: null,
+	},
 }
 
 const discussionSlice = createSlice({
-  name: 'chat',
-  initialState,
-  reducers: {
-    addMessage: (state, action) => {
-      state.messages.push(action.payload)
-    },
-    setMessages: (state, action) => {
-      state.messages = action.payload
-    },
-    setRightClicked: (state, action: PayloadAction<isRightClickType>) => {
-      state.isRightClicked = action.payload
-    }
-  }
+	name: 'chat',
+	initialState,
+	reducers: {
+		addMessage: (state, action) => {
+			const incoming = action.payload
+			const index = state.messages.findIndex((msg) => msg.id === incoming.id)
+			if (index !== -1) {
+				state.messages[index] = incoming // Update
+			} else {
+				state.messages.push(incoming) // New message
+			}
+		},
+		setMessages: (state, action) => {
+			state.messages = action.payload
+		},
+		setRightClicked: (state, action: PayloadAction<isRightClickType>) => {
+			state.isRightClicked = action.payload
+		},
+	},
 })
 
-export const selectMessages = (state: { discussion: { messages: CreateMessageResponseData[] } }) => state.discussion.messages
-export const selectIsRightClicked = (state: { discussion: { isRightClicked: isRightClickType }}) => state.discussion.isRightClicked
+export const selectMessages = (state: {
+	discussion: { messages: CreateMessageResponseData[] }
+}) => state.discussion.messages
+export const selectIsRightClicked = (state: {
+	discussion: { isRightClicked: isRightClickType }
+}) => state.discussion.isRightClicked
 
-export const { addMessage, setMessages, setRightClicked } = discussionSlice.actions
-export default discussionSlice.reducer 
+export const { addMessage, setMessages, setRightClicked } =
+	discussionSlice.actions
+export default discussionSlice.reducer
