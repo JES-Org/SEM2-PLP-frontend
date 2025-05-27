80f179cf7cedba2a8e47d4989f3b606749586c5d
solomon-mh <solomon.muhye.wd@gmail.com>
2025-05-27 18:02:06 +0300

    Update student profile page: replace icons, adjust layout, and improve button widths

--- src/app/(users)/student/profile/page.tsx
+++ src/app/(users)/student/profile/page.tsx
@@ -5,7 +5,7 @@ import React, { useEffect } from 'react'
 
 import { openDialog } from '@/store/features/studentDialogSlice'
 import { useGetStudentProfileQuery } from '@/store/student/studentApi'
-import { Calendar, CircleUser, Library, Phone } from 'lucide-react'
+import { Calendar, CircleUser, GraduationCap, Landmark, Library, Phone } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { useDispatch } from 'react-redux'
 
@@ -13,6 +13,7 @@ import NonEditableProfileFields from '@/components/NonEditableProfileFields'
 import StudentDeleteDialog from '@/components/StudentDeleteDialog'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
+import { IdCardIcon } from '@radix-ui/react-icons'
 
 const Page = () => {
 	const router = useRouter()
@@ -66,9 +67,9 @@ const Page = () => {
 				</Avatar>
 			</div>
 
-			<div className='container min-h-screen rounded-t-full bg-slate-100 md:bg-white mt-10'>
-				<div className='md:flex justify-around pt-40 md:pt-0 md:pl-60'>
-					<div className='md:flex flex-col md:ml-0 ml-3 items-start mb-8 space-y-8 md:space-y-0'>
+			<div className='container min-h-screen rounded-t-full mt-10'>
+				<div className='md:flex justify-around pt-4 md:pt-0 md:pl-60'>
+					<div className='md:flex flex-col md:ml-0 ml-3 items-start mb-2 space-y-2 md:space-y-0'>
 						<NonEditableProfileFields
 							ProfileFieldItems={{
 								icon: <CircleUser />,
@@ -78,20 +79,20 @@ const Page = () => {
 						/>
 						<NonEditableProfileFields
 							ProfileFieldItems={{
-								icon: <Library />,
+								icon: <Landmark />,
 								text: 'Department',
 								value: profile.batch_details?.department_details?.name || 'N/A',
 							}}
 						/>
 						<NonEditableProfileFields
 							ProfileFieldItems={{
-								icon: <Library />,
+								icon: <IdCardIcon />,
 								text: 'Student ID',
 								value: profile.student_id,
 							}}
 						/>
 					</div>
-					<div className='md:flex flex-col md:ml-0 ml-3 items-start mb-8 space-y-8 md:space-y-0'>
+					<div className='md:flex flex-col md:ml-0 ml-3 items-start mb-8 space-y-2 md:space-y-0'>
 						<NonEditableProfileFields
 							ProfileFieldItems={{
 								icon: <Calendar />,
@@ -108,7 +109,7 @@ const Page = () => {
 						/>
 						<NonEditableProfileFields
 							ProfileFieldItems={{
-								icon: <Library />,
+								icon: <GraduationCap />,
 								text: 'Year & Section',
 								value: `Year ${profile.year}, Section ${profile.section}`,
 							}}
@@ -116,13 +117,13 @@ const Page = () => {
 					</div>
 				</div>
 				<div className='flex flex-col md:flex-row justify-center items-center gap-4 mt-10 w-full'>
-					<Button className='w-full md:w-2/12' onClick={handleSubmit}>
+					<Button className='w-1/2 md:w-2/12' onClick={handleSubmit}>
 						Edit
 					</Button>
 
 					<Button
 						variant='destructive'
-						className='w-full md:w-2/12'
+						className='w-1/2 md:w-2/12'
 						onClick={() => dispatch(openDialog('deleteAccount'))}
 					>
 						Delete Account
